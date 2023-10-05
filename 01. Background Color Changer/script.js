58fdf355f696c0c68bbfdaaadf17a5618aaef9ff
hager-dot <hagerelhwarii6633@gmail.com>
2023-10-05 09:39:28 +0300

    :sparkles: Fix heading color change  bug

--- 01. Background Color Changer/script.js
+++ 01. Background Color Changer/script.js
@@ -1,6 +1,13 @@
+$(function () {
+  $("#color-input").on("change input", function () {
+    // Change body background color.
+    $("body").css("background-color", $(this).val());
 
-  $((function(){
-    $("#color-input").on('change input',function(){
-        $('body').css('background-color',$(this).val())
-    })
-  }))
+    //  Change h1 color to white if body color is black.
+    if ($("#color-input").val() <= "#282828") {
+      $("h1").css("color", "white");
+    } else {
+      $("h1").css("color", "black");
+    }
+  });
+});
