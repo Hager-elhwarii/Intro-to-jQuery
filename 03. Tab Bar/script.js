f31546233c3f235ff571a7726c535676df3b9fda
hager-dot <hagerelhwarii6633@gmail.com>
2023-10-07 12:04:55 +0300

    :sparkles: Finish tab bar

---
+++ 03. Tab Bar/script.js
@@ -0,0 +1,14 @@
+
+
+$(function () {
+  $(".tab").on("click", function () {
+    // Add class so when on click it's color opacity increase.
+    $(this).addClass("is_active").siblings().removeClass("is_active");
+    // Move tab-indicator depending on it's left position.
+    let tabIndicator = $('.tab_indicator');
+    let tabPosition = $(this).position();
+    tabIndicator.css({
+        left: tabPosition.left + 'px',
+    })
+  });
+});
